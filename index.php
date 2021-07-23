d9c102e7fd19c8ac8a675a1f5ffe41a64e562e83
koktebelkrim <koktebelkrim@gmail.com>
2021-07-23 12:54:34 +0300

    Начал заполнять контентом

--- index.php
+++ index.php
@@ -252,19 +252,34 @@
   </div> <!-- modal outcall -->
 
   <div class="modal-overlay" id="boots__overlay">
-    <div class="modal" id="boots__modal">
+    <div class="modal main__modal" id="boots__modal">
       <a class="close-modal" id="boots__close">
         <svg viewBox="0 0 20 20">
           <path fill="#000000"
             d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z">
           </path>
         </svg>
-      </a><!-- close modal boots -->
+      </a>
 
-      <div class="modal-content">
-        Boots content
-      </div><!-- content -->
-    </div><!-- modal boots -->
+      <div class="modal-content main__modal-content">
+        <h3 class="modal__title">Размерная сетка обуви garsing</h3>
+
+        <div class="modal__item">
+          <a class="modal__boots-link" href="#">БОТИНКИ С ВЫСОКИМИ БЕРЦАМИ 1700 «RANGER» (З)</a>
+          <div class="modal__boots-info">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore id libero consectetur sunt cupiditate adipisci laboriosam officia explicabo, odit autem cum repellat quis neque totam illo nemo. Atque, minima excepturi.
+            Eligendi veritatis facilis, eum repellat reprehenderit minima placeat dolore quod quibusdam aut numquam consequuntur suscipit et beatae! Iure nisi asperiores modi laboriosam cum, consectetur quidem quae earum, mollitia id porro!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore id libero consectetur sunt cupiditate adipisci laboriosam officia explicabo, odit autem cum repellat quis neque totam illo nemo. Atque, minima excepturi.
+            Eligendi veritatis facilis, eum repellat reprehenderit minima placeat dolore quod quibusdam aut numquam consequuntur suscipit et beatae! Iure nisi asperiores modi laboriosam cum, consectetur quidem quae earum, mollitia id porro!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore id libero consectetur sunt cupiditate adipisci laboriosam officia explicabo, odit autem cum repellat quis neque totam illo nemo. Atque, minima excepturi.
+            Eligendi veritatis facilis, eum repellat reprehenderit minima placeat dolore quod quibusdam aut numquam consequuntur suscipit et beatae! Iure nisi asperiores modi laboriosam cum, consectetur quidem quae earum, mollitia id porro!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore id libero consectetur sunt cupiditate adipisci laboriosam officia explicabo, odit autem cum repellat quis neque totam illo nemo. Atque, minima excepturi.
+            Eligendi veritatis facilis, eum repellat reprehenderit minima placeat dolore quod quibusdam aut numquam consequuntur suscipit et beatae! Iure nisi asperiores modi laboriosam cum, consectetur quidem quae earum, mollitia id porro!</p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
   </div> <!-- modal boots -->
 
   <div class="modal-overlay" id="uniform__overlay">
